d318634f745ef4a6d1ebf3540f0e983ef716569a
tommyb <thomasb841@gmail.com>
2018-06-21 13:25:56 -0400

    adding a few files.

--- JavaWork/Chapter10/RQloop.java
+++ JavaWork/Chapter10/RQloop.java
@@ -2,21 +2,36 @@ import java.util.Scanner;
 
 public class RQloop{
   
-    public static void main(String[]args){
+    public static void main(String[]args) throws Exception{
         Scanner kb = new Scanner(System.in);
         String input;
         char choice;
         boolean isGood = false;
-       do {
-            System.out.println("Do you want to repeat the program or quit?\n(R/Q)");
+       int x = 1;
+       do{
+       try{
+         do {
+            System.out.println("Would you like to"
+           +  " repeat the program?");
             input = kb.nextLine();
             choice = input.charAt(0); 
-            if (choice == 'q' || choice == 'Q'){
+            if (Character.isLetter(choice) &&
+               Character.toUpperCase(choice) == 'Q'){
                 isGood = false;
-              } else if(Character.isLetter(choice)) {
+              } else if(choice == 'r' || choice == 'R') {
+                isGood = true;
+              } else if(Character.isWhitespace(choice) || choice != 'R') {
+               System.out.println("That is not a valid input.");
                 isGood = true;
                 }
          } while (isGood == true);
-        System.out.println("Program over");
+            System.out.println("Program over");
+            x = 2;
       }
+      catch (Exception e){
+        System.out.println("You cant fucking do that idiot." +
+                             " Choose the correct letter."); 
+        }
+       } while (x==1);
+    }
   }
